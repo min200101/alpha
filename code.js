95539d433e1cb3323c7150c0a494b141914fc126
min200101 <80089116+min200101@users.noreply.github.com>
2021-11-25 19:05:36 +0800

    新增了关于我们中的个人介绍模块

---
+++ code.js
@@ -0,0 +1,256 @@
+<section class="w3l-header">
+  <header id="site-header" class="fixed-top">
+    <div class="container">
+      <nav class="navbar navbar-expand-lg navbar-dark stroke">
+        <a class="navbar-brand" href="./contact.html">
+          <span class="fa fa-pencil-square-o"></span> pharmacv
+        </a>
+        <!-- if logo is image enable this
+    <a class="navbar-brand" href="#index.html">
+        <img src="image-path" alt="Your logo" title="Your logo" style="height:35px;" />
+    </a> -->
+        <button class="navbar-toggler  collapsed bg-gradient" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02"
+          aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon fa icon-expand fa-bars"></span>
+          <span class="navbar-toggler-icon fa icon-close fa-times"></span>
+          </span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
+          <ul class="navbar-nav ml-auto">
+
+
+
+
+
+            <li class="nav-item @@contact-active">
+              <a class="nav-link" href="">主页 <span class="sr-only">(current)</span></a>
+            </li>
+
+            <!--   萍接你在这里改-->
+<!--   萍接你在这里改-->
+<!--   萍接你在这里改-->
+<!--   萍接你在这里改-->
+
+<!--   萍接你在这里改-->
+<!--   萍接你在这里改-->
+<!--   萍接你在这里改-->
+<!--   萍接你在这里改-->
+<!--   萍接你在这里改-->
+<!--   萍接你在这里改-->
+<!--   萍接你在这里改-->
+<!--   萍接你在这里改-->
+
+
+          </ul>
+        </div>
+        <!-- toggle switch for light and dark theme -->
+        <div class="mobile-position">
+          <label class="theme-selector">
+            <input type="checkbox" id="themeToggle">
+            <i class="gg-sun"></i>
+            <i class="gg-moon"></i>
+          </label>
+        </div>
+        <!-- //toggle switch for light and dark theme -->
+      </nav>
+    </div>
+  </header>
+  <!--/header-->
+</section>
+<!-- index-block1 -->
+<div class="w3l-index1">
+  <div class="content">
+    <div class="container">
+      <div class="row align-items-center">
+        <div class="col-lg-6 content-left">
+          <h3>pharmacv  <br> 一个专注药价数据做整合查询的平台
+          </h3>
+          <p class="mt-3 mb-lg-5 mb-4">Committed to transparency in drug prices</p>
+
+        </div>
+      </div>
+      <div class="clear"></div>
+    </div>
+  </div>
+</div>
+<!-- //index-block1 -->
+
+
+<!-- index-block2 -->
+
+<!-- /index-block2 -->
+
+
+<!-- stats -->
+<!-- //stats -->
+
+<!-- //logos Section -->
+
+
+<!-- courses Section -->
+
+<section class="w3l-courses py-5">
+  <div class="container py-lg-3">
+    <div class="header-section mb-5 text-center mx-auto">
+      <h3>管理组和数据组</h3>
+      <p class="my-3">负责推进项目 和 获取药品数据</p>
+    </div>
+    <div class="owl-courses owl-carousel owl-theme">
+      <div class="item">
+        <div class="course-grid">
+          <div class="course-img">
+            <img src="../images/tangjingting.png" alt=" " class="img-fluid">
+          </div>
+          <div class="course-cont">
+            <span class="subject">唐劲霆</span>
+            <h5 class="card-title"><a href="courses-single.html">负责项目管理和推进</h5>
+            <a class="blog-btn mt-3">知道更多 <span class="fa fa-long-arrow-right" aria-hidden="true"></span></a>
+          </div>
+        </div>
+      </div>
+
+
+
+
+      <div class="item">
+        <div class="course-grid">
+          <div class="course-img">
+            <img src="../images/chenshuo.png" alt=" " class="img-fluid">
+          </div>
+          <div class="course-cont">
+            <span class="subject">陈硕</span>
+            <h5 class="card-title"><a href="courses-single.html">负责爬取整合数据</a></h5>
+            <a  class="blog-btn mt-3">知道更多 <span class="fa fa-long-arrow-right" aria-hidden="true"></span></a>
+          </div>
+        </div>
+      </div>
+
+
+
+      <div class="item">
+        <div class="course-grid">
+          <div class="course-img">
+            <img src="../images/huangtingsong.png" alt=" " class="img-fluid">
+          </div>
+          <div class="course-cont">
+            <span class="subject">黄艇淞</span>
+            <h5 class="card-title"><a href="courses-single.html">负责爬取整合数据</a></h5>
+            <a  class="blog-btn mt-3">知道更多 <span class="fa fa-long-arrow-right" aria-hidden="true"></span></a>
+          </div>
+        </div>
+      </div>
+
+
+
+
+      <div class="item">
+        <div class="course-grid">
+          <div class="course-img">
+            <img src="../images/suweihuang.png" alt=" " class="img-fluid">
+          </div>
+          <div class="course-cont">
+            <span class="subject">苏伟煌</span>
+            <h5 class="card-title"><a href="courses-single.html">负责项目管理和推进</a></h5>
+            <a  class="blog-btn mt-3">知道更多 <span class="fa fa-long-arrow-right" aria-hidden="true"></span></a>
+          </div>
+        </div>
+      </div>
+
+
+
+      <div class="item">
+        <div class="course-grid">
+          <div class="course-img">
+            <img src="../images/chenbenyuan.png" alt=" " class="img-fluid">
+          </div>
+          <div class="course-cont">
+            <span class="subject">陈本源</span>
+            <h5 class="card-title"><a href="courses-single.html">负责爬取整合数据</a></h5>
+            <a class="blog-btn mt-3">知道更多 <span class="fa fa-long-arrow-right" aria-hidden="true"></span></a>
+          </div>
+        </div>
+      </div>
+
+
+
+
+
+
+
+  </div>
+</section>
+
+<section class="w3l-courses py-5">
+  <div class="container py-lg-3">
+    <div class="header-section mb-5 text-center mx-auto">
+      <h3>前端后端组</h3>
+      <p class="my-3"> 整合数据，搭建web网站，供用户查询使用</p>
+    </div>
+    <div class="owl-courses owl-carousel owl-theme">
+      <div class="item">
+        <div class="course-grid">
+          <div class="course-img">
+            <img src="../images/linzhihuang.png" alt=" " class="img-fluid">
+          </div>
+          <div class="course-cont">
+            <span class="subject">林志煌</span>
+            <h5 class="card-title"><a href="courses-single.html">前端攻城师</a></h5>
+            <a  class="blog-btn mt-3">知道更多 <span class="fa fa-long-arrow-right" aria-hidden="true"></span></a>
+          </div>
+        </div>
+      </div>
+
+
+
+      <div class="item">
+        <div class="course-grid">
+          <div class="course-img">
+            <img src="../images/shizhibin.png" alt=" " class="img-fluid">
+          </div>
+          <div class="course-cont">
+            <span class="subject">石致彬</span>
+            <h5 class="card-title"><a href="courses-single.html">后端攻城师</a></h5>
+            <a href="#" class="blog-btn mt-3">知道更多 <span class="fa fa-long-arrow-right" aria-hidden="true"></span></a>
+          </div>
+        </div>
+      </div>
+
+
+      <div class="item">
+        <div class="course-grid">
+          <div class="course-img">
+            <img src="../images/98.png" alt=" " class="img-fluid">
+          </div>
+          <div class="course-cont">
+            <span class="subject">翁敏</span>
+            <h5 class="card-title"><a href="courses-single.html">前端攻城师</a></h5>
+            <a  class="blog-btn mt-3">知道更多 <span class="fa fa-long-arrow-right" aria-hidden="true"></span></a>
+          </div>
+        </div>
+      </div>
+
+
+
+      <div class="item">
+        <div class="course-grid">
+          <div class="course-img">
+            <img src="../images/wangyiping.png" alt=" " class="img-fluid">
+          </div>
+          <div class="course-cont">
+            <span class="subject">王毅萍</span>
+            <h5 class="card-title"><a href="courses-single.html">全栈攻城师，tql！</h5>
+            <a class="blog-btn mt-3">知道更多 <span class="fa fa-long-arrow-right" aria-hidden="true"></span></a>
+          </div>
+        </div>
+      </div>
+
+
+
+
+
+
+
+
+  </div>
+</section>
